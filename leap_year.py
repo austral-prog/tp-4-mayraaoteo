2e951245038d76fa833e61d47c944f376e30906b
mayraaoteo <162721507+mayraaoteo@users.noreply.github.com>
2024-04-12 11:31:04 -0300

    Listoo leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+
+    my_variable_year = int(input("Ingrese un año: "))
+    
+    if ((my_variable_year % 100 == 0) and (my_variable_year % 400 == 0)) or ( (my_variable_year % 100 != 0) and (my_variable_year % 4) == 0 ):
+        print(f"El año {my_variable_year} es bisiesto")
+    else: print(f"El año {my_variable_year} no es bisiesto")
